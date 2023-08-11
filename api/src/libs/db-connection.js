6a544fdbe06050727415818544a6784b896ab3bd
SantiArguello <Saanti_arguello@hotmail.com>
2023-08-11 20:52:18 -0300

    conneccion a la base de datos mongoAtlas

--- api/src/libs/db-connection.js
+++ api/src/libs/db-connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/appTalleres', {
+    await mongoose.connect('mongodb+srv://saantiarguello:kh2ObO1uL7Qi9IYP@apptalleres.6ko4lhi.mongodb.net/', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
