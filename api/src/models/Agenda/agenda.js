37e78bcb61303a8cb4fc9c44e057addb644000ef
Petri36 <petriniagustin41521@gmail.com>
2023-08-14 21:12:11 -0300

    añadidos modelos de agenda, moto filtrado y motos

---
+++ api/src/models/Agenda/agenda.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const agendaSchema = new mongoose.Schema({
+  fecha: {
+    type: Date,
+    required: true,
+  },
+  nombre: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Cliente",
+  },
+  motoFiltrado: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "motoFiltrado",
+  },
+  trabajo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "trabajoFiltrado",
+  },
+});
+
+const Agenda = mongoose.model("Agenda", agendaSchema);
+
+module.exports = Agenda;
