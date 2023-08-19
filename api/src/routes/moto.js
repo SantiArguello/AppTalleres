b9171fc5ac9c07e84ae734baea97a1043c931ae8
Petri36 <petriniagustin41521@gmail.com>
2023-08-19 18:48:28 -0300

    Pushea la nueva moto al ID del cliente

--- api/src/routes/moto.js
+++ api/src/routes/moto.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const Moto = require("../models/Motos/moto");
+const Cliente = require("../models/Clientes/cliente");
 
 router.post("/moto", async (req, res) => {
   try {
     const nuevaMoto = new Moto(req.body);
     await nuevaMoto.save();
+
+    // ID de la nueva moto al cliente asociado
+    const clienteId = nuevaMoto.cliente;
+    const cliente = await Cliente.findById(clienteId);
+
+    if (!cliente) {
+      return res.status(404).json({ message: "Cliente no encontrado" });
+    }
+
+    cliente.moto.push(nuevaMoto._id);
+    await cliente.save();
+
     res.status(201).json(nuevaMoto);
   } catch (error) {
     res
