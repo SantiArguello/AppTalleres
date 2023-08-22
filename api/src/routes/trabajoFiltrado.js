704b67717106366a3dd232edec6101e501835a29
Petri36 <petriniagustin41521@gmail.com>
2023-08-22 20:26:20 -0300

    Delete de trabajos filtrados creado

--- api/src/routes/trabajoFiltrado.js
+++ api/src/routes/trabajoFiltrado.js
@@ -46,6 +46,27 @@ router.put("/trabajoFiltrado/:id", async (req, res) => {
 
     
 })
- 
+
+router.delete("/trabajoFiltrado/:id", async (req, res) => {
+  try {
+    const trabajoFiltradoId = req.params.id;
+
+    const trabajoFiltradoEliminado = await TrabajoFiltrado.findByIdAndDelete(
+      trabajoFiltradoId
+    );
+    if (!trabajoFiltradoEliminado) {
+      throw new Error("Trabajo no encontrado");
+    }
+
+    res
+      .status(200)
+      .json({ message: "Trabajo eliminado exitosamente" });
+  } catch (error) {
+    res.status(404).json({
+      message: "Error al eliminar el trabajo",
+      error: error.message,
+    });
+  }
+});
 
 module.exports = router;
