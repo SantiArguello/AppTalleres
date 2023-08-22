c7ab15af9458ee4862f32aabe9c152d9e0e81a2b
Petri36 <petriniagustin41521@gmail.com>
2023-08-22 20:26:06 -0300

    Delete de repuestos e insumos creado

--- api/src/routes/repuestosInsumos.js
+++ api/src/routes/repuestosInsumos.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const Repuesto = require("../models/RepuestosInsumos/repuestoInsumo.js");
 
-
 // Ruta para crear un nuevo repuesto o insumo
 
 router.post("/repuesto", async (req, res) => {
@@ -54,5 +53,24 @@ router.put("/repuesto/:id", async (req, res) => {
     });
   }
 });
+
+router.delete("/repuesto/:id", async (req, res) => {
+  try {
+    const repuestoId = req.params.id;
+
+    const repuestoEliminado = await Repuesto.findByIdAndDelete(repuestoId);
+    if (!repuestoEliminado) {
+      throw new Error("Repuesto/Insumo no encontrado");
+    }
+
+    res.status(200).json({ message: "Repuesto/Insumo eliminado exitosamente" });
+  } catch (error) {
+    res.status(404).json({
+      message: "Error al eliminar el repuesto/insumo",
+      error: error.message,
+    });
+  }
+});
+
 
 module.exports = router;
