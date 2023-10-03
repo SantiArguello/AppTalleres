d7db28a7698efc666e4fbef56fa7d2e176253212
lllMaxMaxlll <max.herr.88@gmail.com>
2023-10-03 16:42:13 -0300

    fix: transition sidebar

--- client/src/layout/SidebarMobile.tsx
+++ client/src/layout/SidebarMobile.tsx
@@ -7,17 +7,19 @@ const SidebarMobile = ({ ...props }) => {
 	const { mobileSidebarOpen, handleMobileSidebar } = props;
 
 	return (
-		<div className={`fixed inset-0 flex z-30 md:hidden ${mobileSidebarOpen ? "visible" : "invisible"}`}>
+		<div className={`fixed inset-0 flex z-30 md:hidden ${mobileSidebarOpen ? "visible opacity-100" : "invisible opacity-0"}`}>
 			{/* Overlay */}
 			<div
-				className={`overlay fixed inset-0 bg-neutral-600 bg-opacity-75 ${mobileSidebarOpen ? "visible" : "opacity-0 invisible"}`}
+				className={`fixed inset-0 transition-opacity duration-300 bg-neutral-600 bg-opacity-75 ${
+					mobileSidebarOpen ? "visible opacity-100" : "opacity-0 invisible"
+				}`}
 				onClick={handleMobileSidebar}></div>
 
 			{/* Navbar */}
 
 			<div
 				className={`fixed top-0 flex-1 flex flex-col w-56 bg-inherit md:hidden transition duration-300 ${
-					mobileSidebarOpen ? "translate-0" : "-translate-x-full"
+					mobileSidebarOpen ? "translate-0 visible" : "-translate-x-full opacity-0"
 				} `}>
 				<div className="relative h-screen flex flex-col pt-5 pb-4 bg-neutral-100 dark:bg-neutral-700">
 					<button className="absolute top-0 -right-16 m-4" onClick={handleMobileSidebar}>
