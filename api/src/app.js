4b0479e9496ad79f7397f1d7f37a718d537f365c
SantiArguello <Saanti_arguello@hotmail.com>
2023-10-04 20:10:21 -0300

    cors corregido

--- api/src/app.js
+++ api/src/app.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const connectToDatabase = require("./libs/db-connection");
 const morgan = require("morgan");
+const cors = require('cors');
 const app = express();
 
 // Conectar a la base de datos
 connectToDatabase();
-
 // Configurar middlewares
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(
     cors({
-        origin: "http://localhost:5173",
-    })
-)
+         origin: '*'
+     })
+ )
+
 
 // Importar y usar tus rutas centralizadas desde routes/index.js
 const routes = require("./routes");
