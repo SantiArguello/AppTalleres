dfa0c9cdb3372cb80707afd92927744e990e6e78
SantiArguello <Saanti_arguello@hotmail.com>
2023-10-03 21:02:56 -0300

    correcion del modelo Service - creacion del middleware para el calculo de precios de service

---
+++ api/src/middlewares/sumaPreciosService.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const sumaPreciosService = async function (next) {
+  // Calcular la suma de precioExtra
+  const precioExtraSum = this.precioExtra || 0;
+
+  // Calcular la suma de los precios de repuestos
+  let repuestosSum = 0;
+  if (this.cantidadRepuestos && this.cantidadRepuestos.length > 0) {
+    for (const repuesto of this.cantidadRepuestos) {
+      const repuestoDoc = await mongoose.model('RepuestoInsumo').findById(repuesto.repuesto);
+      if (repuestoDoc) {
+        repuestosSum += repuestoDoc.precio * repuesto.cantidad;
+      }
+    }
+  }
+
+  // Calcular precioRepuestosyExtras
+  this.precioRepuestosyExtras = repuestosSum + precioExtraSum;
+
+  // Calcular precioFinal
+  this.precioFinal = this.manoDeObra + this.precioRepuestosyExtras;
+
+  next();
+};
+
+module.exports = sumaPreciosService;
