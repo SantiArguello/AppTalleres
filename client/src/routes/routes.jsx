c480b44bc8f4e247fc7dfb5b686ecff719746d25
Eloy 666 <63592975+mgaDesarrollo@users.noreply.github.com>
2023-10-09 14:56:52 -0300

    Add files via upload

---
+++ client/src/routes/routes.jsx
@@ -0,0 +1,65 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Layout } from "../layout";
+import { ErrorNotFound } from "../components";
+import { Clientes, Dashboard, Trabajos, Calendario, Facturacion, Notificaciones, Estadisticas, Login, Signup } from "../pages";
+import App from "../App";
+import NuevoService from "../components/NuevoService";
+
+const router = createBrowserRouter([
+	{
+		path: "/login",
+		element: <Login />,
+	},
+	{
+		path: "/signup",
+		element: <Signup />,
+	},
+	{
+		path: "/",
+		element: <Layout />,
+		errorElement: <ErrorNotFound />,
+		children: [
+			{
+				path: "/",
+				index: true,
+				element: <App />,
+			},
+			{
+				path: "dashboard",
+				element: <Dashboard />,
+			},
+			{
+				path: "trabajos",
+				element: <Trabajos />,
+			},
+			{
+				path: "calendario",
+				element: <Calendario />,
+			},
+			{
+				path: "clientes",
+				element: <Clientes />,
+			},
+			{
+				path: "/facturacion",
+				element: <Facturacion />,
+			},
+			{
+				path: "/notificaciones",
+				element: <Notificaciones />,
+			},
+			{
+				path: "/estadisticas",
+				element: <Estadisticas />,
+			},
+			{
+				path: "/nuevo-service",
+				element: <NuevoService />
+
+			},
+			
+		],
+	},
+]);
+
+export default router;
