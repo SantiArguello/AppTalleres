18b39c8d485b56b055c2bd414560ba1aeda6f0e4
SantiArguello <Saanti_arguello@hotmail.com>
2023-10-10 22:21:52 -0300

    Cambios solicitados por el front en rutas get y post de clientes

--- api/src/routes/cliente/postCliente.js
+++ api/src/routes/cliente/postCliente.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const Cliente = require("../../models/Clientes/cliente.js");
+const Moto = require("../../models/Motos/moto.js");
 
 // Ruta para crear un nuevo cliente
 router.post("/cliente", async (req, res) => {
   try {
-    const nuevoCliente = new Cliente(req.body);
-    await nuevoCliente.save();
+    const { nombre, apellido, telefono, moto, correo } = req.body;
+    
+    const nuevoCliente = new Cliente({ nombre, apellido, telefono, correo })
+    const nuevaMoto = new Moto(moto)
+    await nuevaMoto.save()
+    nuevoCliente.moto.push(nuevaMoto._id)
+    await nuevoCliente.save()
+
+    const motoId = await Moto.findByIdAndUpdate(nuevaMoto._id, { cliente: nuevoCliente._id }, { new: true })
+    
+    await motoId.save()
+    
+
+    
+    
+
     res.status(201).json(nuevoCliente);
   } catch (error) {
     res
