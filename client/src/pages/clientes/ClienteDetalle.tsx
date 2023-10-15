d220487503b17479420addad259611a2245d1f56
lllMaxMaxlll <max.herr.88@gmail.com>
2023-10-15 20:48:56 -0300

    fix: boton volver cliente/detalle

--- client/src/pages/clientes/ClienteDetalle.tsx
+++ client/src/pages/clientes/ClienteDetalle.tsx
@@ -1,7 +1,7 @@
-import { Form, useLoaderData } from "react-router-dom";
+import { Form, useLoaderData, useNavigate } from "react-router-dom";
 import { Cliente } from "../../utils/interfaces";
 import { TbUserEdit, TbUserMinus } from "react-icons/tb";
-import { MdWhatsapp, MdOutlineEmail } from "react-icons/md";
+import { MdWhatsapp, MdOutlineEmail, MdArrowBack } from "react-icons/md";
 
 const statusStyles = {
 	finalizado: "bg-green-100 text-green-800 dark:bg-green-700 dark:text-green-100",
@@ -9,11 +9,10 @@ const statusStyles = {
 	fallido: "bg-neutral-100 text-neutral-800 dark:bg-neutral-700 dark:text-neutral-100",
 };
 
-const transactions = [
+const trabajos = [
 	{
 		id: 1,
 		name: "Service completo",
-		href: "#",
 		amount: "$20,000",
 		status: "trabajando",
 		date: "11 Septiembre, 2023",
@@ -22,7 +21,6 @@ const transactions = [
 	{
 		id: 2,
 		name: "Reparacion de motor",
-		href: "#",
 		amount: "$20,000",
 		status: "finalizado",
 		date: "25 Agosto, 2023",
@@ -32,6 +30,8 @@ const transactions = [
 
 const ClienteDetalle = () => {
 	const { cliente } = useLoaderData() as { cliente: Cliente };
+
+	const navigate = useNavigate();
 	// const [showModal, setShowModal] = useState(false);
 	// const [confirmDelete, setConfirmDelete] = useState(false);
 
@@ -49,14 +49,19 @@ const ClienteDetalle = () => {
 
 	return (
 		<>
-			{/* Page header */}
-			<div className="flex items-center justify-center md:justify-end mb-4">
+			<div className="flex items-center justify-between mb-4">
+				<button
+					className="ms-2 rounded-md shadow py-2 px-2 hover:bg-green-600 hover:text-neutral-50"
+					title="Volver"
+					onClick={() => navigate(-1)}>
+					<MdArrowBack size={20} />
+				</button>
 				<div className="flex md:mt-0 md:ml-4 space-x-3">
 					<Form method="delete">
 						<button
 							type="submit"
 							title="Eliminar usuario"
-							className="flex items-center py-2 px-3 rounded-md shadow-sm text-sm leading-4 font-medium  hover:bg-red-600 hover:text-neutral-100  dark:hover:bg-red-700 outline-none border-none focus:ring-2 focus:ring-red-500">
+							className="flex items-center py-2 px-3 rounded-md shadow text-sm leading-4 font-medium  hover:bg-red-600 hover:text-neutral-100  dark:hover:bg-red-700 outline-none border-none focus:ring-2 focus:ring-red-500">
 							<TbUserMinus size={20} />
 							<span className="hidden lg:block ml-2">Eliminar usuario</span>
 						</button>
@@ -65,7 +70,7 @@ const ClienteDetalle = () => {
 						<button
 							type="button"
 							title="Editar usuario"
-							className="flex items-center py-2 px-3 rounded-md shadow-sm text-sm leading-4 font-medium  hover:bg-green-600 hover:text-neutral-100  dark:hover:bg-green-700 outline-none border-none focus:ring-2 focus:ring-green-000">
+							className="flex items-center py-2 px-3 rounded-md shadow text-sm leading-4 font-medium  hover:bg-green-600 hover:text-neutral-100  dark:hover:bg-green-700 outline-none border-none focus:ring-2 focus:ring-green-000">
 							<TbUserEdit size={20} />
 							<span className="hidden lg:block ml-2">Editar usuario</span>
 						</button>
@@ -89,7 +94,7 @@ const ClienteDetalle = () => {
 							title="Enviar whatsapp"
 							aria-label="Whatsapp"
 							target="_blanck"
-							className="relative inline-flex items-center px-4 py-2 border border-neutral-300 dark:border-neutral-500 shadow-sm text-sm font-medium rounded-md hover:bg-green-600 hover:text-neutral-100  focus:outline-none focus:border-none focus:ring-2 focus:ring-green-500 cursor-pointer">
+							className="relative inline-flex items-center px-4 py-2 shadow text-sm font-medium rounded-md hover:bg-green-600 hover:text-neutral-100  focus:outline-none focus:border-none focus:ring-2 focus:ring-green-500 cursor-pointer">
 							<MdWhatsapp className="-ml-1 mr-2 h-5 w-5 -400" aria-hidden="true" />
 							<span>Whatsapp</span>
 						</a>
@@ -98,7 +103,7 @@ const ClienteDetalle = () => {
 							title={`Enviar email a ${cliente.correo}`}
 							aria-label="Email"
 							aria-describedby="email-description"
-							className="ml-3 relative inline-flex items-center px-4 py-2 border border-neutral-300 dark:border-neutral-500 shadow-sm text-sm font-medium rounded-md  hover:bg-blue-500 hover:text-neutral-100 focus:outline-none focus:ring-2 focus:border-none focus:ring-blue-500 cursor-pointer">
+							className="ml-3 relative inline-flex items-center px-4 py-2 shadow text-sm font-medium rounded-md hover:bg-blue-500 hover:text-neutral-100 focus:outline-none focus:ring-2 focus:border-none focus:ring-blue-500 cursor-pointer">
 							<MdOutlineEmail className="-ml-1 mr-2 h-5 w-5 -400" aria-hidden="true" />
 							<span>Email</span>
 						</a>
@@ -113,7 +118,7 @@ const ClienteDetalle = () => {
 					{/* Tabla para mobile  */}
 					<div className="shadow sm:hidden">
 						<ul role="list" className="mt-2 divide-y divide-neutral-200 dark:divide-neutral-600 overflow-hidden shadow sm:hidden">
-							{transactions.map((transaction) => (
+							{trabajos.map((transaction) => (
 								<li key={transaction.id}>
 									<div className="block px-4 py-4  hover:bg-neutral-100 dark:hover:bg-neutral-700 bg-neutral-50 dark:bg-neutral-800">
 										<span className="flex items-center space-x-4">
@@ -156,7 +161,7 @@ const ClienteDetalle = () => {
 											</tr>
 										</thead>
 										<tbody className=" divide-y divide-neutral-200 dark:divide-neutral-600">
-											{transactions.map((transaction) => (
+											{trabajos.map((transaction) => (
 												<tr key={transaction.id} className="">
 													<td className="max-w-0 w-full px-6 py-4 whitespace-nowrap text-sm">
 														<div className="flex">
